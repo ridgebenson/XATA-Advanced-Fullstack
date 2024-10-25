4941c18fdd975f8037be4e73cbc9674a1abcb7bc
BillyJoseph kimani <133921772+billyjoseph1@users.noreply.github.com>
2024-10-25 09:40:13 +0300

    test

--- backend/middlewares/AuthMiddleware.ts
+++ backend/middlewares/AuthMiddleware.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { getXataClient } from '../src/xata';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const client = getXataClient();
